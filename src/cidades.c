4cc05fd279c14cd28d7bd05a1e3a96ed781862d1
vinicius <viniamenic@gmail.com>
2024-10-17 19:33:20 -0300

    incializando repositório e projeto de 'Estrada de Wakanda'

---
+++ src/cidades.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cidades.h"
+
+//decalrações
+
+//protótipos de funções
+
+
+//funções
+int main()
+{
+
+}
+
+
+Estrada *getEstrada(const char *nomeArquivo)
+{
+
+}
+
+
+double calcularMenorVizinhanca(const char *nomeArquivo)
+{
+
+}
+
+
+char *cidadeMenorVizinhanca(const char * nomeArquivo)
+{
+    
+}
